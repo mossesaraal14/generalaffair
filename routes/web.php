78b82ea8c2128c1c551506320e238ddc590bfdd4
Mosses <araalpha14@gmail.com>
2026-07-15 15:50:55 +0700

    rolemiddleware created

--- routes/web.php
+++ routes/web.php
@@ -27,41 +27,50 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     // admin
-    // master atk
-    Route::get('/admin', [AtkController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/admin/atk', [AtkController::class, 'atkIndex'])->name('admin.atk');
-    Route::get('/admin/atk/create', [AtkController::class, 'atkCreate'])->name('admin.atk.create');
-    Route::post('/admin/atk/store', [AtkController::class, 'atkStore'])->name('admin.atk.store');
-    Route::get('/admin/atk/edit/{id}', [AtkController::class, 'atkEdit'])->name('admin.atk.edit');
-    Route::put('/admin/atk/update/{id}', [AtkController::class, 'atkUpdate'])->name('admin.atk.update');
-    Route::get('/admin/atk/delete/{id}', [AtkController::class, 'atkDestroy'])->name('admin.atk.destroy');
+    Route::middleware(['auth', 'role:admin'])->group(function () {
+        // master atk
+        Route::get('/admin', [AtkController::class, 'dashboard'])->name('admin.dashboard');
+        Route::get('/admin/atk', [AtkController::class, 'atkIndex'])->name('admin.atk');
+        Route::get('/admin/atk/create', [AtkController::class, 'atkCreate'])->name('admin.atk.create');
+        Route::post('/admin/atk/store', [AtkController::class, 'atkStore'])->name('admin.atk.store');
+        Route::get('/admin/atk/edit/{id}', [AtkController::class, 'atkEdit'])->name('admin.atk.edit');
+        Route::put('/admin/atk/update/{id}', [AtkController::class, 'atkUpdate'])->name('admin.atk.update');
+        Route::get('/admin/atk/delete/{id}', [AtkController::class, 'atkDestroy'])->name('admin.atk.destroy');
 
-    // transaksi atk
-    Route::get('/admin/atk/transaksi', [AtkController::class, 'atkTransaksi'])->name('admin.atk.transaksi');
-    Route::get('/admin/atk/transaksi/create', [AtkController::class, 'atkTransaksiCreate'])->name('admin.atk.transaksi.create');
-    Route::post('/admin/atk/transaksi/store', [AtkController::class, 'atkTransaksiStore'])->name('admin.atk.transaksi.store');
+        // transaksi atk
+        Route::get('/admin/atk/transaksi', [AtkController::class, 'atkTransaksi'])->name('admin.atk.transaksi');
+        Route::get('/admin/atk/transaksi/create', [AtkController::class, 'atkTransaksiCreate'])->name('admin.atk.transaksi.create');
+        Route::post('/admin/atk/transaksi/store', [AtkController::class, 'atkTransaksiStore'])->name('admin.atk.transaksi.store');
 
-    // tickets
-    Route::get('/admin/tickets/', [TicketController::class, 'index'])->name('admin.tickets');
-    Route::get('/admin/tickets/create', [TicketController::class, 'ticketCreate'])->name('admin.tickets.create');
-    Route::post('/admin/tickets/store', [TicketController::class, 'ticketStore'])->name('admin.tickets.store');
+        // tickets
+        Route::get('/admin/tickets/', [TicketController::class, 'index'])->name('admin.tickets');
+        Route::get('/admin/tickets/create', [TicketController::class, 'ticketCreate'])->name('admin.tickets.create');
+        Route::post('/admin/tickets/store', [TicketController::class, 'ticketStore'])->name('admin.tickets.store');
 
-    // get ticket
-    Route::get('/admin/getticket', [TicketController::class, 'getTicketIndex'])->name('admin.tickets.get');
-    Route::get('/admin/getticket/create', [TicketController::class, 'getTicketCreate'])->name('admin.tickets.get.create');
-    Route::post('/admin/getticket/store', [TicketController::class, 'getTicketStore'])->name('admin.tickets.get.store');
-    Route::get('/admin/getticket/edit/{id}', [TicketController::class, 'getTicketEdit'])->name('admin.tickets.get.edit');
-    Route::post('/admin/getticket/update/{id}', [TicketController::class, 'getTicketUpdate'])->name('admin.tickets.get.update');
+        // get ticket
+        Route::get('/admin/getticket', [TicketController::class, 'getTicketIndex'])->name('admin.tickets.get');
+        Route::get('/admin/getticket/create', [TicketController::class, 'getTicketCreate'])->name('admin.tickets.get.create');
+        Route::post('/admin/getticket/store', [TicketController::class, 'getTicketStore'])->name('admin.tickets.get.store');
+        Route::get('/admin/getticket/edit/{id}', [TicketController::class, 'getTicketEdit'])->name('admin.tickets.get.edit');
+        Route::post('/admin/getticket/update/{id}', [TicketController::class, 'getTicketUpdate'])->name('admin.tickets.get.update');
+    });
 
     // user
-    Route::get('/user', [AtkController::class, 'userDashboard'])->name('user.dashboard');
-    Route::get('/user/tickets', [UserTicketController::class, 'index'])->name('user.tickets');
-    Route::get('/user/tickets/create', [UserTicketController::class, 'ticketCreate'])->name('user.tickets.create');
-    Route::post('/user/tickets/store', [UserTicketController::class, 'ticketStore'])->name('user.tickets.store');
+    Route::middleware(['auth', 'role:user'])->group(function () {
+        // tickets
+        Route::get('/user', [AtkController::class, 'userDashboard'])->name('user.dashboard');
+        Route::get('/user/tickets', [UserTicketController::class, 'index'])->name('user.tickets');
+        Route::get('/user/tickets/create', [UserTicketController::class, 'ticketCreate'])->name('user.tickets.create');
+        Route::post('/user/tickets/store', [UserTicketController::class, 'ticketStore'])->name('user.tickets.store');
+    });
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // anyone who've logged on
+    Route::middleware('auth')->group(function() {
+        // profile
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
