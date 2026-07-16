56f9d8d27e8b6f7c6d059a979017ceac75bb4358
Mosses <araalpha14@gmail.com>
2026-07-16 15:44:10 +0700

    footer modified

--- resources/views/layouts/userlte.blade.php
+++ resources/views/layouts/userlte.blade.php
@@ -196,7 +196,7 @@
   @yield('content')
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2026 <a href="https://berkahrositamandiri.com">PT Berkah Rosita Mandiri</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
